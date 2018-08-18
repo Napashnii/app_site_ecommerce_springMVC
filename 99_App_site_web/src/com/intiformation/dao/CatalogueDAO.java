936085372c0f5a47ce5c330b3d24fef4d09a7574
Heloise abrial <abrialheloise@gmail.com>
2018-08-18 20:21:39 +0200

    partie dao

--- 99_App_site_web/src/com/intiformation/dao/CatalogueDAO.java
+++ 99_App_site_web/src/com/intiformation/dao/CatalogueDAO.java
@@ -1,5 +1,70 @@
 package com.intiformation.dao;
 
-public class CatalogueDAO {
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.intiformation.entity.Categorie;
+
+@Repository
+public class CatalogueDAO implements ICatalogue {
+
+	// Déclaration de la sessionFactory d'hibernate
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	// ajout dans la bdd
+	@Override
+	@Transactional
+	public void addCategorie(Categorie pCategorie) {
+		sessionFactory.getCurrentSession().save(pCategorie);
+
+	}
+
+	@Override
+	@Transactional
+	public void updateCategorie(Categorie pCategorie) {
+		sessionFactory.getCurrentSession().update(pCategorie);
+
+	}
+
+	// suppression
+	@Override
+	@Transactional
+	public void deleteCategorie(int pIdCategorie) {
+		Categorie categorie = sessionFactory.getCurrentSession().get(Categorie.class, pIdCategorie);
+
+		sessionFactory.getCurrentSession().delete(categorie);
+	}
+
+	@Override
+	@Transactional
+	public Categorie getCategorieById(int pIdCategorie) {
+		return sessionFactory.getCurrentSession().get(Categorie.class,pIdCategorie);
+	}
+
+	@Override
+	@Transactional
+	public List<Categorie> getAllCategorie() {
+		
+		return null;
+	}
+	
+
+	/**
+	 * Setter de la sessionFactory pour l'injection de Spring
+	 * @param sessionFactory
+	 */
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
 
 }
+
+
+
--- 99_App_site_web/src/com/intiformation/dao/ICatalogue.java
+++ 99_App_site_web/src/com/intiformation/dao/ICatalogue.java
@@ -1,5 +1,16 @@
 package com.intiformation.dao;
 
+import java.util.List;
+
+import com.intiformation.entity.Categorie;
+
 public interface ICatalogue {
+	
+	public void addCategorie(Categorie pCategorie);
+	public void updateCategorie(Categorie pCategorie);
+	public void deleteCategorie(int pIdCategorie);
+	public Categorie getCategorieById(int pIdCategorie);
+	public List<Categorie> getAllCategorie();
+	
 
 }

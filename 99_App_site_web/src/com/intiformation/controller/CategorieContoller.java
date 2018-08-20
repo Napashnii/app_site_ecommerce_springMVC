77a892d8a0dc4b98e49a81e0d58a62bd72711c10
christelle chou kan pio <christelle.ckp973@gmail.com>
2018-08-20 10:20:31 +0200

    ajout de @controller

--- 99_App_site_web/src/com/intiformation/controller/CategorieContoller.java
+++ 99_App_site_web/src/com/intiformation/controller/CategorieContoller.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.intiformation.entity.Categorie;
 import com.intiformation.service.ICatalogueService;
 
+@Controller
 public class CategorieContoller {
 
 	// declaration du service
